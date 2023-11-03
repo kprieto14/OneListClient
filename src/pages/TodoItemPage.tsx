287a0feac7ec9a61cb6b499525dae830a3512d55
Kristy Prieto <kprieto14@gmail.com>
2023-11-02 20:56:43 -0400

    Add ability to delete toDoItem when accessing it individually through address bar

--- src/pages/TodoItemPage.tsx
+++ src/pages/TodoItemPage.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { TodoItemType } from "../App";
 import axios from "axios";
 
 export function TodoItemPage() {
     const params = useParams<{ id:string }>()
+    const history = useNavigate()
     
     const [todoItem, setTodoItem] = useState<TodoItemType>({
         id: undefined,
@@ -18,22 +19,33 @@ export function TodoItemPage() {
         function () {
           async function loadItems() {
             const response = await axios.get(
-              `https://one-list-api.herokuapp.com/items/${params.id}?access_token=cohort42`
+              `https://one-list-api.herokuapp.com/items/${params.id}?access_token=kristy-prieto`
       )
             if (response.status === 200) {
-              setTodoItem(response.data)
+                setTodoItem(response.data)
       } }
           loadItems()
         },
         [params.id]
-      )
+    )
+
+    async function deleteTodoItem() {
+       const response = await axios.delete(
+            `https://one-list-api.herokuapp.com/items/${params.id}?access_token=kristy-prieto`
+        )
+
+        if (response.status === 204) {
+            // Send the user back to the homepage
+            history("/")
+        }
+    }
 
     return (
         <div>
             <p className={todoItem.complete ? 'completed' : ''}>{todoItem.text}</p>
-            <p>Created: {todoItem.created_at}</p>
-            <p>Updated: {todoItem.updated_at}</p>
-            <button>Delete</button>
+            <p>Created: {todoItem.created_at?.toString()}</p>
+            <p>Updated: {todoItem.updated_at?.toString()}</p>
+            <button onClick={deleteTodoItem}>Delete</button>
         </div>
     )
 }
